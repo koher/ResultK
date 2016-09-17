86859feb3f0e199ef881c652736ec30e48c61031
Yuta Koshizawa <koher@koherent.org>
2016-09-17 11:08:47 +0900

    Add `ResultK` prefix to `precedencegroup`s

--- Sources/Operators.swift
+++ Sources/Operators.swift
@@ -1,23 +1,23 @@
-precedencegroup MonadicPrecedenceRight {
+precedencegroup ResultKMonadicPrecedenceRight {
     associativity: right
     lowerThan: LogicalDisjunctionPrecedence
     higherThan: AssignmentPrecedence
 }
 
-precedencegroup MonadicPrecedenceLeft {
+precedencegroup ResultKMonadicPrecedenceLeft {
     associativity: left
     lowerThan: LogicalDisjunctionPrecedence
     higherThan: AssignmentPrecedence
 }
 
-precedencegroup ApplicativePrecedence {
+precedencegroup ResultKApplicativePrecedence {
     associativity: left
     higherThan: LogicalConjunctionPrecedence
     lowerThan: NilCoalescingPrecedence
 }
 
-infix operator >>- : MonadicPrecedenceLeft
-infix operator -<< : MonadicPrecedenceRight
+infix operator >>- : ResultKMonadicPrecedenceLeft
+infix operator -<< : ResultKMonadicPrecedenceRight
 
-infix operator <^> : ApplicativePrecedence
-infix operator <*> : ApplicativePrecedence
+infix operator <^> : ResultKApplicativePrecedence
+infix operator <*> : ResultKApplicativePrecedence
